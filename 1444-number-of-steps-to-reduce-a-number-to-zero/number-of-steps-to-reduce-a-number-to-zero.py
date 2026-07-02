a3535d0e9b52fb92b6b9d5e5e747e33adf749940
lakshmansavireddy13 <lakshmansavireddy13@gmail.com>
2026-07-02 11:12:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (61.65%) - LeetSync

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfSteps(self, n: int) -> int:
+        count=0
+        while n!=0:
+            if n%2==0:
+                n=n/2
+            else:
+                n=n-1
+            count=count+1
+        return count
